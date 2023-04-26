30777de907684229216f5224d734e916206d73bd
ThomasLe <88428250+ThomasLe-dev@users.noreply.github.com>
2023-04-26 16:33:42 +0700

    web socket configuration for real-time messsages done

--- Backend/server.js
+++ Backend/server.js
@@ -32,6 +32,43 @@ app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
 })
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('setup', (userData) => {
+    socket.join(userData._id);
+    socket.emit('connected');
+  })
+
+  socket.on('join chat', (room) => {
+    socket.join(room);
+    console.log('user joined room ' + room);
+  })
+
+  socket.on('new message', (newMessageReceived) => {
+    const chat = newMessageReceived.chat;
+
+    if(!chat.users){
+      console.log('chat.users is undefined');
+    }
+
+    chat.users.forEach(user => {
+      if(user._id == newMessageReceived.sender._id){
+        return ;
+      }
+
+      socket.in(user._id).emit('message received', newMessageReceived)
+    });
+  });
+});
